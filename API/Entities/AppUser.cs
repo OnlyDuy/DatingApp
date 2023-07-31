5b5cf9d5be12143c55369132c37e5f84ec55de9d
OnlyDuy <vuquangduy002@gmail.com>
2023-07-31 16:00:25 +0700

    Update database Users

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,5 +3,7 @@ namespace API.Entites
     public class AppUser {
         public int Id { get; set; }
         public string ?UserName { get; set; }
+        public byte[] ?PasswordHash { get; set; }
+        public byte[] ?PasswordSalt { get; set; }
     }
 }
