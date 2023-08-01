e2b5f58e7cacd1ec590fd554afc8199fd017f32e
OnlyDuy <vuquangduy002@gmail.com>
2023-08-01 11:27:14 +0700

    Add the create token logic

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,7 +2,7 @@ namespace API.Entites
 {
     public class AppUser {
         public int Id { get; set; }
-        public string ?UserName { get; set; }
+        public string UserName { get; set; } = default!;
         public byte[] PasswordHash { get; set; } = default!;
         public byte[] PasswordSalt { get; set; } = default!;
     }
