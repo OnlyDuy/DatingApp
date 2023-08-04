605aa42dfaf467d5bec07ab3e86499d92840f697
OnlyDuy <vuquangduy002@gmail.com>
2023-08-04 23:15:59 +0700

    Adding a server error page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { MessagesComponent } from "./messages/messages.component";
 import { AuthGuard } from "./_guards/auth.guard";
 import { TestErrorsComponent } from "./errors/test-errors/test-errors.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
+import { ServerErrorComponent } from "./errors/server-error/server-error.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -24,7 +25,8 @@ const routes: Routes = [
     },
     {path: 'errors', component: TestErrorsComponent},
     {path: 'not-found', component: NotFoundComponent},
-    {path: '**', component: HomeComponent, pathMatch: 'full'},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path: '**', component: NotFoundComponent, pathMatch: 'full'},
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-
+    ServerErrorComponent,
+    
   ],
   imports: [
     BrowserModule,
