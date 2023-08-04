9720cdf63b19e44550afc8c16a1e3456b12de9e5
OnlyDuy <vuquangduy002@gmail.com>
2023-08-04 17:19:08 +0700

    Adding an error interceptor

---
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error => {
+        if (error) {
+          switch (error.status) {
+            case 400:
+              if (error.error.errors) {
+                const modalStateErors = [];
+                for (const key of error.error.errors) {
+                  if (error.error.errors[key]) {
+                    //push: Nối các phần tử mới vào cuối một mảng và trả về độ dài mới của mảng.
+                    modalStateErors.push(error.error.errors[key]);
+                  }
+                }
+                throw modalStateErors;
+              } else {
+                this.toastr.error(error.statusText, error.status);
+              }
+              break;
+            case 401:
+              this.toastr.error(error.statusText, error.status);
+              break;
+            case 404:
+              this.router.navigateByUrl('/not-found');
+              break;
+            case 500:
+              // Ở đây dùng bộ định tuyến để chuyển đổi trạng thái
+              const navigationExtras: NavigationExtras = {state: {error: error.error}}
+              this.router.navigateByUrl('/server-error', navigationExtras);
+              break;
+            default:
+              this.toastr.error('Something unexpected went wrong');
+              break;
+          }
+        }
+        // Nếu không bắt được lỗi thì sẽ trả lại lỗi cho bất kỳ thứ gì đang gọi yêu cầu HTTP trong lần đầu tiên
+        return throwError(error);
+      })
+    )
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,9 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
     FormsModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
