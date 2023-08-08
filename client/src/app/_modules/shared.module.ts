f6c7b9e828481e7311cd6fb3d99351045033a4a2
OnlyDuy <vuquangduy002@gmail.com>
2023-08-08 17:48:48 +0700

    Create detail page

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 @NgModule({
@@ -13,11 +13,13 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
+    TabsModule.forRoot(),
   ],
   // Cần export để dropdown
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit{
+  member!: Member; 
 
+  constructor(private memberService: MembersService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (username) {
+      this.loadMember(username);
+    } else {
+      // Xử lý khi không có giá trị username
+      this.router.navigate(['/not-found']);
+    }
+  }
+
+  loadMember(username: string) {
+    this.memberService.getMember(username).subscribe(member => {
+      this.member = member;
+    })
+  }
 }
