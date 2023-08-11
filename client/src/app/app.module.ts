e9d43ae0ab7cc2b9b08b3ad30cd85af435ce3a0d
OnlyDuy <vuquangduy002@gmail.com>
2023-08-11 17:02:00 +0700

    Adding a photo editor component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
+    PhotoEditorComponent,
     
   ],
   imports: [
