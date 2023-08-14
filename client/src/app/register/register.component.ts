f218af8e906533579ffabff340e134260e784e36
OnlyDuy <vuquangduy002@gmail.com>
2023-08-14 15:36:19 +0700

    Validation feedback

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -24,7 +24,7 @@ export class RegisterComponent implements OnInit {
 
   intitializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required,
         Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
