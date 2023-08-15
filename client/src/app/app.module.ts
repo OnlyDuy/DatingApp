e97343c55a6acbdc2c9f9ab3e7b700ad06f736fa
OnlyDuy <vuquangduy002@gmail.com>
2023-08-15 23:12:52 +0700

    Creating a reusable text input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -44,6 +45,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     MemberCardComponent,
     MemberEditComponent,
     PhotoEditorComponent,
+    TextInputComponent,
     
   ],
   imports: [
