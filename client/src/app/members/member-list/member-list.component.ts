639684775e3af72b105e6231a1ecb13f16205ca7
OnlyDuy <vuquangduy002@gmail.com>
2023-08-18 17:48:19 +0700

    Adding filter buttons to the client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -17,6 +17,7 @@ export class MemberListComponent implements OnInit{
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
 
   constructor(private memberService: MembersService, private accountService: AccountService) {
     // Người dùng hiện tại của AccountService sẽ sử dụng đường dẫn ở đây
@@ -35,6 +36,11 @@ export class MemberListComponent implements OnInit{
       this.members = response.result;
       this.pagination = response.pagination;
     })
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.user);
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
