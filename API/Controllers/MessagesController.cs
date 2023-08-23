c4718369a9b5c7c9baee68030c8130dbc3078d3c
OnlyDuy <vuquangduy002@gmail.com>
2023-08-23 13:30:47 +0700

    Adding a message controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,58 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.interfaces;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessageController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        public MessageController(IUserRepository userRepository, IMessageRepository messageRepository,
+             IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            // Lấy tên người dùng
+            var username = User.GetUsername();
+            // kiểm tra tên người dungf có bằng với tên người nhận trong CreateMessageDto không
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                return BadRequest("You cannot send messages to yourself");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            // Nếu không có nhười nhận, kiểm tra điều này và xem liệu nó có rỗng không
+            if (recipient == null) return NotFound();
+
+            // tạo tin nhắn mới và lưu trữ tin nhắn 
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to save message");
+        }   
+    }
+}
