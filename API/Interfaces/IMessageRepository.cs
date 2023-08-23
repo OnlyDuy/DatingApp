c724673a636c6cded5fb5c2e28a15b077b905292
OnlyDuy <vuquangduy002@gmail.com>
2023-08-23 10:14:11 +0700

    Setting up the message repository

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+using API.Helpers;
+using API.DTOs;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
