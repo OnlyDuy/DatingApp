ec06fb8650ba2ddc134851df4521288f7882f320
OnlyDuy <vuquangduy002@gmail.com>
2023-08-26 16:17:59 +0700

    Fixing the photo weirdness

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -12,9 +12,10 @@ export class MessagesComponent implements OnInit {
 
   messages: Message[] = [];
   pagination: Pagination;
-  container = 'Outbox';
+  container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) {}
 
@@ -23,9 +24,11 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
+      this.loading = false;
     })
   }
 
